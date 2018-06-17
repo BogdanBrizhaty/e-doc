d891ba9b1d94fac1744ef7a96ab3fe9be64eb10e
Богдан Брижатый <demaskinas@yandex.ua>
2018-06-18 02:33:49 +0300

    [ED-3] - common objects. Common controller with Mapper inside

---
+++ src/eDoc.Web/Controllers/Base/ControllerCommon.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eDoc.Web.Controllers.Base
+{
+    public class ControllerCommon : Controller
+    {
+        private IMapper _mapper = null;
+
+        protected IMapper mapper
+        {
+            get
+            {
+                if (_mapper == null)
+                    _mapper = DependencyResolver.Current.GetService<IMapper>();
+
+                return _mapper;
+            }
+        }
+    }
+}
