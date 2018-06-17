47521860194ea7d35ea568ac42cfd50adc4f251c
Богдан Брижатый <demaskinas@yandex.ua>
2018-06-18 02:50:14 +0300

    [ED-1] - moved profile base to Model

---
+++ src/eDoc.Model/Data/Context/ApplicationContextBase.cs
@@ -0,0 +1,29 @@
+﻿using eDoc.Model.Data.Entities;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eDoc.Model.Data.Context
+{
+    public abstract class ApplicationContextBase : IdentityDbContext<ApplicationUserBase>
+    {
+        public ApplicationContextBase()
+            : this("DefaultConnection")
+        {
+        }
+
+        public ApplicationContextBase(string connStringName)
+            :base(connStringName, throwIfV1Schema: false)
+        {
+
+        }
+
+        public static TContextType Create<TContextType>() where TContextType : class
+        {
+            return Activator.CreateInstance(typeof(TContextType)) as TContextType;
+        }
+    }
+}
--- src/eDoc.Web/Controllers/AccountController.cs
+++ src/eDoc.Web/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using eDoc.Web.Models;
+using eDoc.Model.Data.Entities;
 
 namespace eDoc.Web.Controllers
 {
@@ -151,7 +152,7 @@ namespace eDoc.Web.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUserBase { UserName = model.Email, Email = model.Email };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
@@ -367,7 +368,7 @@ namespace eDoc.Web.Controllers
                 {
                     return View("ExternalLoginFailure");
                 }
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUserBase { UserName = model.Email, Email = model.Email };
                 var result = await UserManager.CreateAsync(user);
                 if (result.Succeeded)
                 {
