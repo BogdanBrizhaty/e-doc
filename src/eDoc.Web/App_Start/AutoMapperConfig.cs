a83d4ff53eebd897c7bf4a155695326536f42f01
Богдан Брижатый <demaskinas@yandex.ua>
2018-06-17 23:17:58 +0300

    ED-2 - AutoMapper integration

---
+++ src/eDoc.Web/App_Start/AutoMapperConfig.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using MapperProfileBase = eDoc.Web.AutoMapper.MapperProfileBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+namespace eDoc.Web.App_Start
+{
+    public static class AutoMapperConfig
+    {
+        public static void Initialize()
+        {
+            Mapper.Initialize(cfg => GetConfig());
+        }
+
+        private static MapperConfiguration GetConfig()
+        {
+            var profiles = Assembly
+                .GetExecutingAssembly()
+                .GetTypes()
+                .Where(t => t.IsAssignableFrom(typeof(MapperProfileBase)) && t != typeof(MapperProfileBase))
+                .ToList();
+
+            var config = new MapperConfiguration(
+                cfg =>
+                {
+                    profiles.ForEach(p => cfg.AddProfile(Activator.CreateInstance(p) as Profile));
+                });
+
+            return config;
+        }
+    }
+}
---
+++ src/eDoc.Web/AutoMapper/ProfileBase.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace eDoc.Web.AutoMapper
+{
+    public abstract class MapperProfileBase : Profile
+    {
+        public MapperProfileBase()
+        {
+            CreateProfile();
+        }
+        protected abstract void CreateProfile();
+    }
+}
