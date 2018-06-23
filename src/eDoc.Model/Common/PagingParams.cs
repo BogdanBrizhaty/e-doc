05ca804829f22f462e6a48241dd908f05be2f189
Богдан Брижатый <demaskinas@yandex.ua>
2018-06-24 01:25:35 +0300

    ED-6 - update paging params

--- src/eDoc.Model/Common/PagingParams.cs
+++ src/eDoc.Model/Common/PagingParams.cs
@@ -12,5 +12,27 @@ namespace eDoc.Model.Common
         public int PerPage { get; }
         public int Fetch { get; }
         public int Offset { get; }
+
+        public PagingParams(int page, int perPage)
+            :this(page, perPage, perPage)
+        {
+        }
+        public PagingParams(int page, int perPage, int fetch)
+            :this(page, perPage, fetch, 0)
+        {
+        }
+        public PagingParams(int page, int perPage, int fetch, int offset)
+        {
+            if (page < 1 || perPage < 1 || fetch < 1 || offset < 1)
+                throw new ArgumentOutOfRangeException();
+
+            if (offset > fetch - 1)
+                throw new ArgumentException("Offset and Fetch are not valid");
+
+            Page = page;
+            PerPage = perPage;
+            Fetch = fetch;
+            Offset = offset;
+        }
     }
 }
